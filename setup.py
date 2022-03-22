18955094a5889b1946dcbc9a67437344ab95dada
Philippe Gris <gris@clermont.in2p3.fr>
2022-03-22 16:43:17 +0100

    add description info

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# get the version here
+pkg_vars = {}
+
+with open("version.py") as fp:
+    exec(fp.read(), pkg_vars)
+
+setup(
+    name='ztf_cadence',
+    version=pkg_vars['__version__'],
+    description='a set of scripts to perform cadence studies',
+    url='http://github.com/pgris/ztf_cadence',
+    author='Ph.Gris',
+    author_email='philippe.gris@clermont.in2p3.fr',
+    license='BSD',
+    packages=['ztf_cadence'],
+    python_requires='>=3.5',
+    zip_safe=False,
+    install_requires=[
+        'ztf_pipeutils>=0.1',
+        'healpy>=1.13.0'
+    ],
+)
