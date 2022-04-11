5f2a644974d368cd535668f05dc80f49ff05376b
Manon <manon30270@gmail.com>
2022-04-11 18:05:45 +0200

    loop for all the pixel

--- ztf_metrics/metric.py
+++ ztf_metrics/metric.py
@@ -205,7 +205,7 @@ def read(input_dir, fileName):
     cl = CadenceMetric()   
 
     df1 = pd.DataFrame()
-    for b in bo_ok[10:20]:
+    for b in bo_ok:
         df_ = df[df['healpixID'].str.contains(b)]
         df_new = df_.copy()
         pd_ = cl.run(df_new)
