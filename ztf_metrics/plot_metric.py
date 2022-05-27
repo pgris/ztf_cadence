635a9a6c57e072c1f3473d2a45960cf76d5801fe
Philippe Gris <gris@clermont.in2p3.fr>
2022-05-27 17:23:17 +0200

    add nround parameter for display

--- ztf_metrics/plot_metric.py
+++ ztf_metrics/plot_metric.py
@@ -7,6 +7,7 @@ import pandas as pd
 from matplotlib import cm
 import seaborn as sns
 
+
 class PlotOS:
     """
     class to plot OS pointings vs time
@@ -16,9 +17,11 @@ class PlotOS:
       nside for healpix (default: 128)
     outDir: str,opt
       output directory (default: plot_cadence)
+    hold: bool, opt
+      to make multi var on the same plot (default: False)
     """
 
-    def __init__(self, nside=128, outDir='plot_cadence'):
+    def __init__(self, nside=128, outDir='plot_cadence', hold=False, nround=2):
 
         # get npixels
         self.npix = hp.nside2npix(nside=nside)
@@ -26,7 +29,9 @@ class PlotOS:
         self.cmap.set_extremes(under='w', over='gray')
         self.outDir = outDir
         self.pixArea = hp.nside2pixarea(nside, degrees=True)
-        
+        self.hold = hold
+        self.nround = nround
+
     def visu(self, tab, vardisp='color', healpixId='healpixID', title='', inum=1, save=False, min_tab=0, max_tab=100,
              cbar=True):
         """
@@ -46,28 +51,29 @@ class PlotOS:
         """
 
         self.cbar = cbar
-        idx = tab['healpixID']=='None'
+        idx = tab['healpixID'] == 'None'
         self.sel = tab[~idx]
         self.min_ = min_tab
         self.max_ = max_tab
         norm = plt.cm.colors.Normalize(self.min_, self.max_)
-    
+
         hpxmap = np.zeros(self.npix, dtype=np.float)
         hpxmap += -1
         healpixIDs = np.unique(self.sel[healpixId])
         hpxmap[self.sel['healpixID'].astype(int)] = self.sel[vardisp]
-        
-        mk_sup = self.sel[vardisp]<max_tab
-        mk_inf = self.sel[vardisp]>min_tab
+
+        mk_sup = self.sel[vardisp] < max_tab
+        mk_inf = self.sel[vardisp] > min_tab
         mk_born = mk_sup & mk_inf
-        
+
         tab_masked = self.sel[mk_born]
 
-        title += ' / median = {} \n moy = {}'.format(round(tab_masked[vardisp].median(), 1), round(tab_masked[vardisp].mean(), 1))
-        
+        title += ' / median = {} \n moy = {}'.format(
+            round(tab_masked[vardisp].median(), self.nround), round(tab_masked[vardisp].mean(), self.nround))
+
         ik = hpxmap > 1.
         area = self.pixArea*len(hpxmap[ik])
-        
+
         hp.mollview(hpxmap, min=self.min_, max=self.max_, cmap=self.cmap,
-                    title=title, nest=True, norm=norm, cbar=self.cbar, hold=True)
+                    title=title, nest=True, norm=norm, cbar=self.cbar, hold=self.hold)
         hp.graticule(dpar=10., dmer=30.)
