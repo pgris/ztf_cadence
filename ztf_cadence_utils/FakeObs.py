8e3d0bd3aaa69dbe4b9ae3b6b9a8881791c0f4d7
Manon <manon30270@gmail.com>
2022-06-30 15:27:58 +0200

    some modif

--- ztf_cadence_utils/FakeObs.py
+++ ztf_cadence_utils/FakeObs.py
@@ -4,79 +4,39 @@ import numpy as np
 from random import randint
 from random import seed
 
-def generateObsData(cad_g, cad_r, cad_i, skynoise_g, skynoise_r, skynoise_i, MJD_min, zp, healpixID_min, healpixID_max, step, seed):
+def generateObsData(cad_g, cad_r, cad_i, skynoise_g, skynoise_r, skynoise_i, MJD_min, zp_g, zp_r, zp_i, healpixID_min,
+                    healpixID_max, step, seed):
     
-    df = pd.DataFrame()
-    df_r = pd.DataFrame()
-    df_i = pd.DataFrame()
     df_tot = pd.DataFrame()
 
-    MJD = np.arange(MJD_min, MJD_min+180, cad_g)
-    healpixID = np.arange(healpixID_min, healpixID_max, step)
-    
-    for pix in healpixID:
+    MJD = [np.arange(MJD_min, MJD_min+180, cad_g), np.arange(MJD_min+(15/(25*60)), MJD_min+(15/(25*60))+180, cad_r),
+          np.arange(MJD_min+(30/(25*60)), MJD_min+(30/(25*60))+180, cad_i)]
+    healpixID = 1
+    skynoise = [skynoise_g, skynoise_r, skynoise_i]
+    zp = [zp_g, zp_r, zp_i] 
+    band = ['ztfg','ztfr', 'ztfi']
         
-        np.random.seed(seed)
-    
-        df['time'] = MJD 
-        df['field'] = np.random.randint(250, 1866)
-        df['skynoise'] = skynoise_g
-        df['zp'] = zp
+    for i, band in enumerate(band):
+        df = pd.DataFrame()
+        print(band)
+
+        df['time'] = MJD[i]
+        df['field'] = 701.0
+        df['skynoise'] = skynoise[i]
+        df['zp'] = zp[i]
         df['rcid'] = 1.0
-        df['band'] = 'ztfg'
-        #df['ccd_x'] = 1.0
+        df['band'] = band
         df['programid'] = 1.0
-        df['ra'] = np.random.randint(35, 72)
-        df['dec'] = np.random.randint(-27, 79)
-        df['i'] = -1.0
+        df['ra'] = 59.955157
+        df['dec'] = 41.037876
+        df['i'] = 0
         df['j'] = -1.0
-        df['ra_quad'] = df['ra']
-        df['dec_quad'] = df['dec']
-        #df['ccd_y'] = 1.0
-        df['healpixID'] = pix
+        df['ra_quad'] = 60.921218
+        df['dec_quad'] = 40.927792
+        df['healpixID'] = healpixID
         df['healpixID']= df['healpixID'].map(str)
 
-        MJD_r = np.arange(MJD_min+(15/(25*60)), MJD_min+(15/(25*60))+180, cad_r)
-
-        df_r['time'] = MJD_r
-        df_r['field'] = np.random.randint(250, 1866)
-        df_r['skynoise'] = skynoise_r
-        df_r['zp'] = zp
-        df_r['rcid'] = 1.0
-        df_r['band'] = 'ztfr'
-        #df_r['ccd_x'] = 1.0
-        df_r['programid'] = 1.0
-        df_r['ra'] = np.random.randint(35, 72)
-        df_r['dec'] = np.random.randint(-27, 79)
-        df_r['i'] = -1.0
-        df_r['j'] = -1.0
-        df_r['ra_quad'] = df_r['ra']
-        df_r['dec_quad'] = df_r['dec']
-        #df_r['ccd_y'] = 1.0
-        df_r['healpixID'] = pix
-        df_r['healpixID']= df_r['healpixID'].map(str)
-
-        MJD_i = np.arange(MJD_min+(30/(25*60)), MJD_min+(30/(25*60))+180, cad_i)
-
-        df_i['time'] = MJD_i
-        df_i['field'] = np.random.randint(250, 1866)
-        df_i['skynoise'] = skynoise_i
-        df_i['zp'] = zp
-        df_i['rcid'] = 1.0
-        df_i['band'] = 'ztfi'
-        #df_i['ccd_x'] = 1.0
-        df_i['programid'] = 1.0
-        df_i['ra'] = np.random.randint(35, 72)
-        df_i['dec'] = np.random.randint(-27, 79)
-        df_i['i'] = -1.0
-        df_i['j'] = -1.0
-        df_i['ra_quad'] = df_i['ra']
-        df_i['dec_quad'] = df_i['dec']
-        #df_i['ccd_y'] = 1.0
-        df_i['healpixID'] = pix
-        df_i['healpixID']= df_i['healpixID'].map(str)
-
-        df_tot = pd.concat([df_tot, df, df_r, df_i])
+        df_tot = pd.concat([df_tot, df])
     
     return df_tot
     
